7b66d4a09ecc0ffa397c4cf8a8dcaf5d8453cd53
jhonf1084 <johfredy1084@gmail.com>
2023-06-13 10:38:34 -0500

    primer commit, creacion de proyecto

---
+++ src/main/java/com/webflux/liquidaciones/LiquidacionesApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.liquidaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidacionesApplication.class, args);
+	}
+
+}
